65f8867b40318e0c4b072e1b64bf2a3a1d055312
vaish0705 <65487258+vaish0705@users.noreply.github.com>
2021-12-27 19:17:46 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (48.79%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+       stack<char>st;
+        
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+            {
+                st.push(s[i]);
+                
+                
+            }
+            
+        
+            
+            else
+ 
+        {
+                
+                
+if(st.empty()  ||  (st.top()=='(' &&  s[i]!=')' )  ||      (st.top()=='{'&& s[i]!='}')  || (st.top()=='[' && s[i] !=']' )) 
+{
+    return false;
+}
+                
+               
+                    st.pop();
+                
+            }
+            
+        }
+            
+            return st.empty();
+            
+            
+            
+        
+        
+    }
+};

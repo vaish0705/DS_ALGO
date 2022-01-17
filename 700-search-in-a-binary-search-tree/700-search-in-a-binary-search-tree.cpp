b9a4a873c3ab8fa5a9d19a133640fb037d1562f3
vaish0705 <65487258+vaish0705@users.noreply.github.com>
2022-01-17 17:34:47 +0530

    Time: 40 ms (76.23%), Space: 34.8 MB (66.34%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -12,58 +12,23 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-       
-        //base case  
-        //if root of the tree is pointing to null we dont need to find the value 
-        //just return null
-       /* if(root==nullptr)
+        
+        if(root==nullptr)
         {
             return NULL;
         }
-        
-        
-        //if root value is greater thn the val
-        //than we need to search in the left subtree  only
-        //bcz value on the right part will be greater thn the root value so there is no point of searching in  right subtree
         
        else if(val<root->val)
         {
            return  searchBST(root->left,val);
         }
         
-        
-        //if root value is lesser thn the val
-        //than we need to search in the right subtree  only
-        //bcz value on the left part will be lesser thn the root value so there is no point of searching in  left subtree  
-        
         else if(val>root->val)
         {
             return searchBST(root->right,val);
         }
         
-        
-        //if we given valexist in the tree  thn return the subtree rooted with that node.
-        //If such a node does not exist, return null
-        return root;*/
-        
-        
-    
-        while(root!=nullptr && root->val!=val)
-        {
-           root= root->val>val?root->left:root->right;
-        }
-        
-        
-        
         return root;
-        
-        
-        
-        
-        
-        
-        
-        
         
         
     }

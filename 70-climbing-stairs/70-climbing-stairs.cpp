dffc12922a62392d47da466f34bb24e841a024d8
vaish0705 <65487258+vaish0705@users.noreply.github.com>
2022-02-05 19:03:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
-       /* if(n==0 ||n==1)
+      /*  
+        if(n==0 ||n==1)
          {
              return 1;
          }
         
-        */
-       // return climbStairs(n-1)+climbStairs(n-2);
+        
+     
          int dp[n+1];
-      /* memset(dp,-1,sizeof(dp));
+      memset(dp,-1,sizeof(dp));
         
         if(dp[n]!=-1)
         {
@@ -20,7 +20,12 @@ public:
         
         
     dp[n]=climbStairs(n-1)+climbStairs(n-2);
-        return dp[n];*/
+        return dp[n];
+       
+        
+        */
+                 int dp[n+1];
+
         
         dp[0]=1;
         dp[1]=1;

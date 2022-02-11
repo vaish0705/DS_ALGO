8fb8e1c3f29c22516f7a82efea975c537286d4f2
vaish0705 <65487258+vaish0705@users.noreply.github.com>
2022-02-11 23:49:14 +0530

    Time: 140 ms (54.30%), Space: 180 MB (7.27%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,32 +1,32 @@
 class MyHashSet {
 public:
-    //we will make a vactor of size 10^6+1
-    //when new elmnt is added we will mark that index as 1
-    //int contain func we need to chk whether the elmnt is prsnt or not
-    //in remove func we just need to chngevalue from 1 to 0
-    
-    
-    vector<int>v;
+            vector<int>s;
     int n;
     MyHashSet() {
-        int n =1e6+1;
-        v.resize(n);
+     n=1e6+1;
         
+           s.resize(n);
     }
     
     void add(int key) {
-        v[key]=1;
+        
+    s[key]=1;  
         
     }
     
     void remove(int key) {
-        v[key]=0;
+        
+            s[key]=0;
+        
         
     }
     
     bool contains(int key) {
-        return v[key];
-        
+        if(s[key]==1)
+        {
+            return true;
+        }
+        return false;
     }
 };
 
